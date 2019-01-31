e75ce5ed786f303e9e5e698ad52edec3597f60db
Zhengguan Li <zhengguan.li-1@ou.edu>
2019-01-31 14:03:46 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,30 @@
+## Calculate unit quarter circle area(Pi/4)
+
+Calculate the unit quarter circle area using `Monte Carlo` method in `Matlab`.
+
+## Get started
+
+### Prerequisite
+
+Set the number of points for each case in `calculateUnitQuarterCircleArea.m`
+
+```
+totalPointCounts = [count1, count2, count3...]
+```
+
+### Usage
+run `calculateUnitQuarterCircleArea.m`
+
+### Testing
+
+```
+totalPointCounts = [100, 1000, 1000]
+```
+
+![](100_points.jpg)
+
+![](1000_points.jpg)
+
+![](10000_points.jpg)
+
+![](line_graph.jpg)
---
+++ calculateUnitQuarterCircleArea.m
@@ -0,0 +1,56 @@
+tic
+
+totalPointCounts = [100, 1000, 10000]; 
+
+len = length(totalPointCounts);
+insidePointCounts = zeros(len, 1);
+area = zeros(len, 1);
+
+%% main program
+j = 0;
+for N = totalPointCounts
+    j = j + 1;
+    
+    % generate N random points with range [0, 1)
+    points = rand(N, 2);
+    
+    % set axis and grid
+    axis([0, 1, 0, 1]);
+    grid on; % show grid
+
+    if (j > 1)
+        figure;
+    end
+    
+    % draw points
+    i = 1:N;
+    plot(points(i, 1), points(i, 2), '*');
+    hold on;
+
+    % draw quarter circle
+    axis square;
+    f = ezplot('x^2+y^2-1=0', [0, 1, 0, 1]);
+    set(f, 'LineWidth', 4, 'color', 'k');
+    
+    % count the number of points inside the quarter circle
+    count = 0;
+    for i = 1:N
+        if ((points(i, 1)^2 + points(i, 2)^2) <= 1)
+            count = count + 1;
+        end
+    end
+    insidePointCounts(j, 1) = count;
+    
+    % calculate Pi: points inside/total points
+    area(j, 1) = insidePointCounts(j, 1) ./ N;
+    area(j, 2) = N;
+    
+    count
+end
+
+%% plot line chart
+figure;
+i = 1:3;
+plot(area(i, 2), area(i, 1), '*-');
+
+toc
